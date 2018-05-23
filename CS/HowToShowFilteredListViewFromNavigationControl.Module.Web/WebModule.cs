7cdecd6257f7e2d74025e391afb482e41a851c24
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/HowToShowFilteredListViewFromNavigationControl.Module.Web/WebModule.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace HowToShowFilteredListViewFromNavigationControl.Module.Web {
+	[ToolboxItemFilter("Xaf.Platform.Web")]
+	public sealed partial class AspNetModule : ModuleBase {
+		public AspNetModule() {
+			InitializeComponent();
+		}
+	}
+}
