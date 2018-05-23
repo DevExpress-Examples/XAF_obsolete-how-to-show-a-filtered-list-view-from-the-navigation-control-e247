3586e960be4c4415fe4c263aa46184324c911ebb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/FilteredListViewNavigationControl.Module/MyShowNavigationItemController.cs
@@ -0,0 +1,42 @@
+using DevExpress.ExpressApp.DC;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Data.Filtering;
+
+namespace HowToShowFilteredListViewFromNavigationControl.Module {
+	public partial class MyShowNavigationItemController : ShowNavigationItemController {
+		protected override void ShowNavigationItem(SingleChoiceActionExecuteEventArgs args) {
+			base.ShowNavigationItem(args);
+			if((args.SelectedChoiceActionItem != null) && args.SelectedChoiceActionItem.Enabled) {
+				if(args.SelectedChoiceActionItem.Caption == "Today's Tasks") {
+					IObjectSpace objectSpace = Application.CreateObjectSpace();
+					CollectionSource collectionSource = (CollectionSource)Application.CreateCollectionSource(objectSpace, typeof(Task), "Task_ListView");
+					//Use the CurrentDate read-only parameter to create a criteria operator			
+					collectionSource.Criteria["CurrentDate"] = CriteriaWrapper.ParseCriteriaWithReadOnlyParameters(
+						"[StartDate] = '@CurrentDate'", typeof(Task));
+					View view = Application.CreateListView("Task_ListView", collectionSource, true);
+					view.CustomizeViewShortcut += new EventHandler<CustomizeViewShortcutArgs>(View_CustomizeViewShortcut);
+					view.Disposing += new CancelEventHandler(view_Disposing);
+					args.ShowViewParameters.CreatedView = view;
+					args.ShowViewParameters.TargetWindow = TargetWindow.Current;
+				}
+			}
+		}
+		void view_Disposing(object sender, CancelEventArgs e) {
+			View view = (View)sender;
+			view.CustomizeViewShortcut -= new EventHandler<CustomizeViewShortcutArgs>(View_CustomizeViewShortcut);
+			view.Disposing -= new CancelEventHandler(view_Disposing);
+		}
+		private void View_CustomizeViewShortcut(object sender, CustomizeViewShortcutArgs e) {
+			e.ViewShortcut.Add("Filter", "On");
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# OBSOLETE - How to Show a Filtered List View from the Navigation Control
+
+
+<p><strong>=========================================</strong><br><strong>This example is now obsolete, because it uses a quite low level API (ModelNodesGeneratorUpdater), which is required in advanced scenarios only. Instead, please implement simpler solutions in code or in the Model Editor:</strong><br><strong><a href="https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113198.aspx">Navigation System | Define the Navigation Structure</a><a href="https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112988.aspx"> <br>Criteria Property of a List View's Collection Source</a></strong><br><strong><a href="https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112990.aspx">Criteria Property in the Application Model</a> </strong><br><strong>=========================================</strong><br><br>This example demonstrates how to show a filtered List View via a navigation control's item. The "Today's Tasks" navigation item will be added. It is intended to show tasks that should be started today.</p>
+<p>For details, refer to the <a href="http://documentation.devexpress.com/#Xaf/CustomDocument2918">How to: Show a Filtered List View from the Navigation Control</a> topic in XAF documentation.</p>
+
+<br/>
+
+
